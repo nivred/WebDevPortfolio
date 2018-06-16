8cc9d44306751a8b9bf6836d3fc72ffbb61ad59b
nivred <emaildervin@gmail.com>
2018-06-16 12:09:18 -0500

    changed form submit btn cursor to pointer

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -26,3 +26,68 @@ $(function() {
 		});
 	});
 });
+
+
+
+
+// const skills = document.querySelectorAll('.devi')
+
+// function scrollReveal(e) {
+// 	console.log(window.scrollY);
+// 	skills.forEach(skill => {
+// 		const revealAt = (window.scrollY + window.innerHeight) - skill.height / 2;
+
+// 		if (revealAt) {
+// 			skills.removeClass('hidden');
+// 			console.log('NOW')
+// 		}
+// 	});
+// }
+
+// window.addEventListener('scroll', scrollReveal());
+
+
+
+// function Scroll() {
+// 	var skills = $('div#skills-list');
+// 	var ypos = window.pageYOffset;
+// 	if(ypos > 1000 && skills.hasClass('hidden')) {
+// 		skills.removeClass('hidden');
+// 	}
+// }
+
+// window.addEventListener('scroll', Scroll());
+
+
+// $(window).scroll(function () {
+//     $('.devi').each(function () {
+//         var imagePos = $(this).offset().top;
+//         var imageHeight = $(this).height();
+//         var topOfWindow = $(window).scrollTop();
+
+//         if (imagePos > topOfWindow + imageHeight) {
+//             $(this).removeClass('hidden');
+// 			}
+//     });
+// });
+
+
+// $(document).ready(function() {
+// 	$(window).scroll(function(event) {
+// 		var y = $(this).scrollTop();
+// 		if (y >= 300) {
+// 			$('#skills-list').removeClass('hidden');
+// 		}
+// 	});
+// });
+
+// (function() {
+// 	const skills = $('div#skills-list');
+// 	const skillsOffset = skills.offset().top/2;
+// 	const doc = $(document);
+
+// 		doc.on('scroll', function() {
+// 			if (doc.scrollTop() > skillsOffset && skills.hasClass('hidden')) skills.removeClass('hidden');
+// 		});
+// })();
+
